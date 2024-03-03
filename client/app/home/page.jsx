9afd9623a2bc7a9ef1eb63df7d4630d3d5f39d28
xenom2004 <117723390+xenom2004@users.noreply.github.com>
2024-03-03 17:05:26 +0530

    stup

---
+++ client/app/home/page.jsx
@@ -0,0 +1,18 @@
+"use client"
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <main>
+    <div className='bg-blue-300 text-xl'>
+        color
+
+      
+    </div>
+    </main>
+    </>
+  )
+}
+
+export default Home;
