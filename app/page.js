ee4f36223fef750a511dbf013ed2c39ffcb7f639
Siddhesh <siddheshwaje123456@gmail.com>
2024-03-08 12:20:56 +0530

    issue resolved

--- app/page.js
+++ app/page.js
@@ -4,11 +4,6 @@ import Logit from "./components/loginbutton";
 export default function Home(){
   return (
     <main>
-      <h1 className="text-7xl">Home Page</h1>
-      <div className="ml-24 pl-24">
-      <Logit/></div>
-      
-
     </main>
   )
 }
