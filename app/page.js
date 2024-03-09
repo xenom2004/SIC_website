19f3a0a00191ce70bce84451207f17072afecaa9
Siddhesh <siddheshwaje123456@gmail.com>
2024-03-09 11:59:55 +0530

    class to className

--- app/page.js
+++ app/page.js
@@ -13,10 +13,10 @@ export default function Home() {
             alt="Welcome"
           />
         </div>
-        <section class="text-black z-10">
-            <div class="container mx-auto px-15 py-16"> 
-              <h1 class="text-5xl font-bold mb-4" role="heading" aria-level="1">Sophisticated Instrumentation Center</h1>
-              <p class="text-lg mb-8">
+        <section className="text-black z-10">
+            <div className="container mx-auto px-15 py-16"> 
+              <h1 className="text-5xl font-bold mb-4" role="heading" aria-level="1">Sophisticated Instrumentation Center</h1>
+              <p className="text-lg mb-8">
                 The Sophisticated Instrumentation Center (SIC) — A National Facility was established in September 2011 to expedite the research program at IIT Indore. It is now a national facility providing services such as data recording facilities and expertise in different state-of-the-art instruments to academia and industry from all parts of the country and some international centers. Furthermore, it is catering to the needs of many educational institutes and industries in and around central India and satisfying the need of scientific world, academia and industries with equal importance and emphasis. It is also engaged in spreading awareness among researchers, academia and industries for the probable use of the facility among diverse users to make the facility a part of our mutual co-existence to enhance quality of researches and products in industries with elan.
               </p>
               <Link href='./about'> 
