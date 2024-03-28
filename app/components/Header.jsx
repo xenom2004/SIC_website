f2e1f5ac2cc1e3c957dda5abb875a7d492832abd
Siddhesh <siddheshwaje123456@gmail.com>
2024-03-29 02:13:19 +0530

    frontend

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -36,74 +36,76 @@ const Header = () => {
   return (
     <header className="text-gray-100 bg-gradient-to-r from-blue-600 via-blue-700 to-indigo-800 body-font shadow-md fixed w-full z-50 top-0">
       {" "}
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
-        <Link href="/" className="flex items-center text-white mb-4 md:mb-0">
-          <img
-            src="https://citc.iiti.ac.in/wp-content/uploads/2021/12/IITILogo.png"
-            className="h-16 px-10"
-            alt="Company Logo"
-          />
-          <span className="text-xl font-bold ml-2">
-            Sophisticated Instrumentation Center
-          </span>
-        </Link>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base">
-          <Link
-            href="/"
-            className="mr-5 font-semibold text-white  hover:text-white"
-          >
-            Home
+      <nav className=" bg-opacity-30 border-b border-blue-700">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+          <Link href="/" className="flex items-center text-white mb-4 md:mb-0">
+            <img
+              src="https://citc.iiti.ac.in/wp-content/uploads/2021/12/IITILogo.png"
+              className="h-16 px-10"
+              alt="Company Logo"
+            />
+            <span className="text-xl font-bold ml-2">
+              Sophisticated Instrumentation Center
+            </span>
           </Link>
-          <div className="mr-5 font-semibold hover:text-white">
-            <Instruments />
-          </div>
-          <div className="mr-5 font-semibold hover:text-white">
-            <About />
-          </div>
-
-          <Link
-            href="/usageCharges"
-            className="mr-5  text-white font-semibold hover:text-white"
-          >
-            Usage Charges
-          </Link>
-          {session && (
+          <nav className="md:ml-auto flex flex-wrap items-center text-base">
             <Link
-              href="/user"
-              className="mr-5  font-semibold text-white  hover:text-white"
+              href="/"
+              className="mr-5 font-semibold text-white  hover:text-white"
             >
-              Orders
+              Home
             </Link>
-          )}
-        </nav>
-        <div className="flex items-center">
-          {session && (
-            <Link href="/profile">
-              <button className=" font-semibold px-4 py-2   bg-blue-850   text-white  mr-4">
-                Profile
+            <div className="mr-5 font-semibold hover:text-white">
+              <Instruments />
+            </div>
+            <div className="mr-5 font-semibold hover:text-white">
+              <About />
+            </div>
+
+            <Link
+              href="/usageCharges"
+              className="mr-5  text-white font-semibold hover:text-white"
+            >
+              Usage Charges
+            </Link>
+            {session && (
+              <Link
+                href="/user"
+                className="mr-5  font-semibold text-white  hover:text-white"
+              >
+                Orders
+              </Link>
+            )}
+          </nav>
+          <div className="flex items-center">
+            {session && (
+              <Link href="/profile">
+                <button className=" font-semibold px-4 py-2   bg-blue-850   text-white  mr-4">
+                  Profile
+                </button>
+              </Link>
+            )}
+            {/* Logout button */}
+            {session && (
+              <button
+                onClick={handleSignOut}
+                className="text-sm font-semibold px-4 py-2 rounded-md bg-red-500 text-white hover:bg-red-600 transition duration-300 ease-in-out transform hover:scale-105"
+              >
+                Logout
               </button>
-            </Link>
-          )}
-          {/* Logout button */}
-          {session && (
-            <button
-              onClick={handleSignOut}
-              className="text-sm font-semibold px-4 py-2 rounded-md bg-red-500 text-white hover:bg-red-600 transition duration-300 ease-in-out transform hover:scale-105"
-            >
-              Logout
-            </button>
-          )}
-          {/* Login/Register */}
-          {!session && (
-            <button
-              onClick={handleSignIn}
-              className="text-sm font-semibold px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105 mr-4"
-            >
-              Login/Register
-            </button>
-          )}
+            )}
+            {/* Login/Register */}
+            {!session && (
+              <button
+                onClick={handleSignIn}
+                className="text-sm font-semibold px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105 mr-4"
+              >
+                Login/Register
+              </button>
+            )}
+          </div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 };
--- app/page.js
+++ app/page.js
@@ -4,28 +4,31 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex-1 relative">
-        <div className="h-screen flex items-center justify-center text-center relative">
-          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-current "></div>
-          <img
-            className="h-full w-full object-cover"
-            src="https://sic.iiti.ac.in/wp-content/uploads/2022/08/cropped-Home-1-Copy.jpg"
-            alt="Welcome"
-          />
-          <section className="absolute top-0 left-0 w-full h-full flex items-center justify-start px-8">
-            <div className="container mx-auto text-white max-w-3xl">
-              <h1 className="text-5xl font-bold mb-4 text-left">Sophisticated Instrumentation Center</h1>
-            
-              <Link href='./about'> 
+    <main className="flex-1 relative">
+      <div className="h-screen flex items-center justify-center text-center relative">
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-current "></div>
+        <img
+          className="h-full w-full object-cover"
+          src="https://sic.iiti.ac.in/wp-content/uploads/2022/08/cropped-Home-1-Copy.jpg"
+          alt="Welcome"
+        />
+        <section className="absolute top-0 left-0 w-full h-full flex items-center justify-start px-8">
+          <div className="container ml-52 mb-28 text-white max-w-3xl">
+            <h1 className="text-5xl font-bold mb-4 text-left font-museo-slab">Sophisticated Instrumentation</h1>
+            <h1 className="text-5xl font-bold mb-4 text-left font-museo-slab">  Center</h1>
+            <h1 className="drop-shadow-lg font-bold mb-4 text-left font-museo-slab">Here you can transform your inspiration into impact.</h1>
+            <h1 className="font-bold mb-4 text-left font-museo-slab">
+              <Link href='./about' > 
                 <button type="button" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                   Learn More
                 </button>
               </Link>
-            </div>
-          </section>
-        </div>
-      </main>
-    </div>
+            </h1>
+          </div>
+        </section>
+      </div>
+    </main>
+  </div>
   );
 }
 
